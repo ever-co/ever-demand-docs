4a4d17ead61766443fd2ff3b1e2374faffcb105d
Ruslan Konviser <evereq@gmail.com>
2018-09-30 17:37:54 +0300

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -24,7 +24,7 @@ const siteConfig = {
   title: 'Ever® Platform', // Title for your website.
   tagline: 'Documentation',
   url: 'https://docs.ever.co', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  baseUrl: '/ever-docs/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
